5c323dc6a8677a70d3e0e15678af4af11f6e0548
jkone27 <jkone27@yahoo.it>
2019-06-16 18:00:23 +0200

    missing files

--- Braili.Web/HttpHandler.fs
+++ Braili.Web/HttpHandler.fs
@@ -4,11 +4,19 @@
     open Giraffe
     open FSharp.Control.Tasks.ContextInsensitive
     open Ocr
+    open System.Drawing.Common
 
     let handleOcrRequest =
         fun (next : HttpFunc) (ctx : HttpContext) ->
         task {
-            let resultText = readTextfromImage
+
+            //if (ctx.Request.ContentType <> "multipart/form-data") then
+            //    let! r = RequestErrors.BAD_REQUEST "unsupported mime" next ctx 
+            //    return r
+            let img = new Image()
+            use imageStream = ctx.Request.Form.Files.[0].OpenReadStream()
+            let test : byte[] = [||]
+            let resultText = readTextfromImage test
             return! text resultText next ctx
         }
             
